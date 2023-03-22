39e368098e9352b3a70512754f5ca19bdf0cf7f7
Kurumi <ayangkurumi@gmail.com>
2023-03-22 07:26:04 +0700

    Can't return when got user rate limited

--- bot/helper/mirror_utils/upload_utils/gdriveTools.py
+++ bot/helper/mirror_utils/upload_utils/gdriveTools.py
@@ -427,7 +427,7 @@ class GoogleDriveHelper:
                     f"Total Attempts: {err.last_attempt.attempt_number}")
                 err = err.last_attempt.exception()
             err = str(err).replace('>', '').replace('<', '')
-            if "User rate limit exceeded" in err:
+            if "User rate limit exceeded" or "userRateLimitExceeded" in err:
                 msg = "Limit harian file tercapai! Coba lagi kapan kapan :D"
             elif "File not found" in err:
                 token_service = self.__alt_authorize()
