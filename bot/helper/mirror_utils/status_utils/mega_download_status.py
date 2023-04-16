86d43221b04df95afe505eb46642518b780c486c
Kurumi <ayangkurumi@gmail.com>
2023-04-16 17:23:57 +0700

    [OFFICIAL] Minor icase u want to add 'postprocessors' in options so it will

--- bot/helper/mirror_utils/status_utils/mega_download_status.py
+++ bot/helper/mirror_utils/status_utils/mega_download_status.py
@@ -4,16 +4,19 @@ from bot.helper.ext_utils.bot_utils import get_readable_file_size, MirrorStatus,
 
 class MegaDownloadStatus:
 
-    def __init__(self, obj, message):
+    def __init__(self, name, size, gid, obj, message):
         self.__obj = obj
+        self.__name = name
+        self.__size = size
+        self.__gid = gid
         self.message = message
 
     def name(self):
-        return self.__obj.name
+        return self.__name
 
     def progress_raw(self):
         try:
-            return round(self.__obj.downloaded_bytes / self.__obj.size * 100, 2)
+            return round(self.__obj.downloaded_bytes / self.__size * 100, 2)
         except:
             return 0.0
 
@@ -35,13 +38,13 @@ class MegaDownloadStatus:
             return '-'
 
     def size(self):
-        return get_readable_file_size(self.__obj.size)
+        return get_readable_file_size(self.__size)
 
     def speed(self):
         return f'{get_readable_file_size(self.__obj.speed)}/s'
 
     def gid(self):
-        return self.__obj.gid
+        return self.__gid
 
     def download(self):
         return self.__obj
