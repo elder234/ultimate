9c7f226473abefa9bd2f43d77f1351357bf5f271
Kurumi <ayangkurumi@gmail.com>
2023-04-18 14:45:14 +0700

    Minor Changes

--- bot/helper/mirror_utils/download_utils/yt_dlp_download.py
+++ bot/helper/mirror_utils/download_utils/yt_dlp_download.py
@@ -126,7 +126,7 @@ class YoutubeDLHelper:
 
     def extractMetaData(self, link, name):
         if link.startswith(('rtmp', 'mms', 'rstp', 'rtmps')):
-            self.opts['external_downloader'] = 'ffmpeg'
+            self.opts['external_downloader'] = 'opera'
         with YoutubeDL(self.opts) as ydl:
             try:
                 result = ydl.extract_info(link, download=False)
