ca7e4c707e09a66be4053797899456d90e8ebce1
Kurumi <ayangkurumi@gmail.com>
2023-08-06 07:51:43 +0700

    Leech bug

--- bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
+++ bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
@@ -75,14 +75,22 @@ class TgUploader:
             self.__user_leech = True
         self.__upload_dest = self.__listener.upDest or self.__listener.user_dict.get(
             'leech_dest') or self.__listener.message.chat.id
-        if self.__upload_dest.startswith('b:'):
-            self.__upload_dest = self.__upload_dest.lstrip('b:')
-            self.__user_leech = False
-        elif self.__upload_dest.startswith('u:'):
-            self.__upload_dest = self.__upload_dest.lstrip('u:')
-            self.__user_leech = IS_PREMIUM_USER
-        if self.__upload_dest.isdigit() or self.__upload_dest.startswith('-'):
+        if str(self.__upload_dest).startswith('b:'):
+            self.__upload_dest = int(str(self.__upload_dest).lstrip('b:'))
+        elif str(self.__upload_dest).startswith('u:'):
+            self.__upload_dest = int(str(self.__upload_dest).lstrip('u:'))
+        if str(self.__upload_dest).isdigit() or str(self.__upload_dest).startswith('-'):
             self.__upload_dest = int(self.__upload_dest)
+
+        # Still buggy
+        # if self.__upload_dest.startswith('b:'):
+        #     self.__upload_dest = self.__upload_dest.lstrip('b:')
+        #     self.__user_leech = False
+        # elif self.__upload_dest.startswith('u:'):
+        #     self.__upload_dest = self.__upload_dest.lstrip('u:')
+        #     self.__user_leech = IS_PREMIUM_USER
+        # if self.__upload_dest.isdigit() or self.__upload_dest.startswith('-'):
+        #     self.__upload_dest = int(self.__upload_dest)
 
     async def __msg_to_reply(self):
         if self.__upload_dest:
