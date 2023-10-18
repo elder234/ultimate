6762041197c594064c9ecc25eeeb6634e0ced79d
Kurumi <ayangkurumi@gmail.com>
2023-10-18 20:08:56 +0700

    Reply for rclone path

--- bot/modules/mirror_leech.py
+++ bot/modules/mirror_leech.py
@@ -164,7 +164,7 @@ async def _mirror_leech(client, message, isQbit=False, isLeech=False, sameDir=No
 
         if file_ is None:
             reply_text = reply_to.text.split('\n', 1)[0].strip()
-            if is_url(reply_text) or is_magnet(reply_text):
+            if is_url(reply_text) or is_magnet(reply_text) or is_rclone_path(reply_text):
                 link = reply_text
             elif reply_to.reply_markup:
                 try:
