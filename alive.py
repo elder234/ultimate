771ed0355b086d9f952812a19f0821d522298e4b
Kurumi <ayangkurumi@gmail.com>
2024-01-29 07:32:43 +0700

    Revert back auto alive

--- alive.py
+++ alive.py
@@ -1,34 +1,71 @@
+from logging import (
+    getLogger,
+    FileHandler,
+    StreamHandler,
+    INFO,
+    basicConfig,
+    error as log_error,
+    warning as log_warning,
+)
+from os import environ
 from requests import get
 from time import sleep
 
-from bot import (
-    LOGGER,
-    BASE_URL,
-    BASE_URL_PORT,
+
+basicConfig(
+    format="{asctime} - [{levelname[0]}] {name} [{module}:{lineno}] - {message}",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    style="{",
+    handlers=[FileHandler("log.txt"), StreamHandler()],
+    level=INFO,
 )
+
+LOGGER = getLogger("AutoAlive")
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:123.0) Gecko/20100101 Firefox/123.0"
 }
 
 try:
-    if (
-        BASE_URL 
-        and BASE_URL_PORT is not None
-    ):
-        while True:
-            try:
-                sleep(500)
-                get(
-                    BASE_URL, 
-                    headers=headers, 
-                    timeout=5
-                )
-            except:
-                pass
-
+    PORT = environ.get("PORT")
+    HEROKU_APP_NAME = environ.get("HEROKU_APP_NAME", "")
+    RENDER_APP_NAME = environ.get("RENDER_APP_NAME", "")
+    if len(HEROKU_APP_NAME) != 0:
+        BASE_URL = f"https://{HEROKU_APP_NAME}.herokuapp.com"
+        if (
+            BASE_URL 
+            and PORT is not None
+        ):
+            while True:
+                try:
+                    sleep(500)
+                    get(
+                        BASE_URL, 
+                        headers=headers, 
+                        timeout=5
+                    )
+                except:
+                    pass
+                
+    elif len(RENDER_APP_NAME) != 0:
+        BASE_URL = f"https://{RENDER_APP_NAME}.onrender.com"
+        if (
+            BASE_URL 
+            and PORT is not None
+        ):
+            while True:
+                try:
+                    sleep(500)
+                    get(
+                        BASE_URL, 
+                        headers=headers, 
+                        timeout=5
+                    )
+                except:
+                    pass
+                
     else:
-        LOGGER.warning("Auto Alive is not set up correctly! Don't forget to add HEROKU_APP_NAME or RENDER_APP_NAME to prevent the Apps got shutdown!")
-
+        log_warning("Auto Alive is not set up correctly! Don't forget to add HEROKU_APP_NAME or RENDER_APP_NAME to prevent the Apps got shutdown!")
+        
 except Exception as e:
-    LOGGER.error(e)
+    log_error(e)
